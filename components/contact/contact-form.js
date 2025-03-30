2ddf535aa8a95eadc5a2b8f001d479d62909ed2b
Zaryab Ahmed Khan <zaryabzak22@gmail.com>
2025-03-31 00:08:45 +0500

    add feedback for form

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,26 +1,88 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
 
 export default function ContactForm() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredName, setEnteredName] = useState("");
   const [enteredMessage, setEnteredMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState(null); //pending, success, error
+  const [error, setError] = useState("");
 
-  function sendMessageHandler(event) {
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  async function sendMessageHandler(event) {
     event.preventDefault();
 
-    // sending request to the api route
-    fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify({
-        email: enteredEmail,
-        name: enteredName,
-        message: enteredMessage,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    // set status to peding
+    setRequestStatus("pending");
+
+    try {
+      // sending request to the api route
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          name: enteredName,
+          message: enteredMessage,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "something went wrong");
+      }
+
+      setRequestStatus("success");
+
+      if (requestStatus === "success") {
+        setEnteredEmail("");
+        setEnteredMessage("");
+        setEnteredName("");
+      }
+    } catch (err) {
+      setError(err);
+      setRequestStatus("error");
+    }
+  }
+
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is on its way",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: "Message sent successfully",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notification = {
+      status: "error",
+      title: "Error!",
+      message: error,
+    };
   }
 
   return (
@@ -71,6 +133,13 @@ export default function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 }
